63ecd9f15f32bba1a555b202e5e77f7b23903945
Candice-Tian <tian779713952@163.com>
2023-02-28 23:24:37 +0800

    Add switch case test

---
+++ src/SwitchCaseTests.java
@@ -0,0 +1,14 @@
+public class SwitchCaseTests {
+    public void patternUsing() {
+        String fruit = "apple";
+        switch (fruit) {
+            case "apple" -> System.out.println("Selected apple");
+            case "pear" -> System.out.println("Selected pear");
+            case "mango" -> {
+                System.out.println("Selected mango");
+                System.out.println("Good choice!");
+            }
+            default -> System.out.println("No fruit selected");
+        }
+    }
+}
